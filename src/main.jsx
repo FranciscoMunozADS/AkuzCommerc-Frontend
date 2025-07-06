3b85455047619d975a46e344cba66cb5af61ab4d
Francisco Muñoz <munozjaraf@gmail.com>
2025-07-05 23:11:11 -0400

    Carrito terminado

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
-      </UserProvider> 
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { profilePage as ProfilePage } from "./Profile/profilePage";
 export { errorPage as ErrorPage } from "./Error/errorPage";
 export { Register as RegisterPage } from "./Register/Register";
 export { Login as LoginPage } from "./Login/Login";
+export { Cart as CartPage } from "./Cart/Cart";
